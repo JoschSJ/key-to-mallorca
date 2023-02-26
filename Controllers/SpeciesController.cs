6c5f3fe39577b17c923645f201d7567f7f26d456
JSJ <97600183+Josch-SJ@users.noreply.github.com>
2023-02-26 18:18:39 +0000

    create plant display page - wip

---
+++ Controllers/SpeciesController.cs
@@ -0,0 +1,36 @@
+﻿using key_to_mallorca_wasm.Models;
+using key_to_mallorca_wasm.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace key_to_mallorca_wasm.Controllers;
+
+public class SpeciesController: ISpeciesDetails
+{
+    public string? Name => _plant != null ? $"{_plant.Genus} {_plant.Species}" : null;
+    public string? CommonName => _plant?.CommonName;
+    public string? Family => _plant?.Family;
+    public string? Image => _plant?.Photo1;
+    public string? ImageCaption => _plant?.Credit1;
+    public string? Description => _plant?.Description;
+
+    [Inject]
+    private SpeciesService SpeciesService { get; set; }
+
+    private SpeciesList? _speciesList;
+    private PlantSpecies? _plant;
+
+    public SpeciesController(SpeciesService speciesService)
+    {
+        SpeciesService = speciesService;
+    }
+
+    public async Task GetPlantSpecies(string name)
+    {
+        name = name.ToLower();
+        var nameSplit = name.Split('_');
+        _speciesList ??= await SpeciesService.GetSpeciesList();
+        _plant = _speciesList.Plant.Find(
+            o => o.Genus.ToLower() == nameSplit[0]
+                 && o.Species.ToLower() == nameSplit[1]);
+    }
+}
---
+++ Models/SpeciesList.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace key_to_mallorca_wasm.Models;
+
+public class SpeciesList
+{
+    public List<PlantSpecies> Plant { get; set; }
+    public List<BirdSpecies> Bird { get; set; }
+}
+public class PlantSpecies
+{
+    public string Group { get; set; }
+    public string Family { get; set; }
+    public string Genus { get; set; }
+    [JsonPropertyName("species")] public string Species { get; set; }
+    [JsonPropertyName("Commonname")] public string CommonName { get; set; }
+    public string Description { get; set; }
+    public string Photo1 { get; set; }
+    public string Credit1 { get; set; }
+    public string Photo2 { get; set; }
+    public string Credit2 { get; set; }
+    public string Photo3 { get; set; }
+    public string Credit3 { get; set; }
+    public string Subfamily { get; set; }
+}
+
+public class BirdSpecies
+{
+    public string Order { get; set; }
+    public string Family { get; set; }
+    public string Commonname { get; set; }
+    public string Genus { get; set; }
+    public string species { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
+    public string Photo1 { get; set; }
+    public string Credit1 { get; set; }
+    public string Photo2 { get; set; }
+    public string Credit2 { get; set; }
+}
